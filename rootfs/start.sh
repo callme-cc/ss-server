80e92f2e7e7c8eec0517c1469003311efb505f9c
root <root@debian>
2016-05-11 21:51:54 +0000

    aa

---
+++ rootfs/start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+workdir=/work
+keydir=/work/key
+
+if [ ! -d $workdir ]; then
+    echo "Create configure file.."
+    mkdir $workdir
+    cp -R /conf/ss-server.conf $workdir
+    mkdir $keydir
+    cd $keydir
+    /usr/sbin/dnscrypt-wrapper --gen-provider-keypair  | grep Public > finger.txt
+    cat $keydir/finger.txt
+    /usr/sbin/dnscrypt-wrapper --gen-cert-file --crypt-secretkey-file=secret.key > dns.txt
+fi
+
+echo "Start ss-server.."
+#/usr/sbin/ss-server -p 8443 -k $K -m $M $F $T
+/usr/sbin/ss-server -c $workdir/ss-server.conf -f /tmp/ss-server.pid
+
+echo "Start dnscrypt-wrapper..."
+/usr/sbin/dnscrypt-wrapper --resolver-address=8.8.8.8:53 --listen-address=0.0.0.0:443 \
+                   --provider-name=dns.baidu.com \
+                   --crypt-secretkey-file=$keydir/secret.key --provider-cert-file=$keydir/dnscrypt.cert
